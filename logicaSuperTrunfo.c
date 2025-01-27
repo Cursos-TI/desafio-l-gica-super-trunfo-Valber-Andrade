49566ee0dac11e42e1649549da00a147ce66a940
Valber-Andrade <168591912+Valber-Andrade@users.noreply.github.com>
2025-01-27 22:45:43 +0000

    Implementando a logica do menu interativo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -15,6 +15,7 @@ int main() {
     float DensidadePopulacional1, DensidadePopulacional2;
     float PibPerCapita1, PibPerCapita2;
     float SuperPoder1, SuperPoder2;
+    int Jogador;
     
     // Cadastro das Cartas:
     //Adicionei um printf para o usuario se orientar a informar os dados da cidade de forma respectiva
@@ -56,47 +57,76 @@ int main() {
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
 
-    if (populacao1 > populacao2) {
+   //Criação do menu interativo
+
+   printf("Jogo do Super Trunfo!\n");
+   printf("Escolha uma das opções para comparar o vencedor:\n");
+   printf("1. População\n");
+   printf("2. Aréa\n");
+   printf("3. PIB\n");
+   printf("4. Pontos turísticos\n");
+   printf("5. Densidade Populacional\n");
+   printf("6. PIB Per Capita\n");
+   printf("7. Super Poder\n");
+   scanf("%d", &Jogador);
+
+   switch (Jogador)
+   {
+   case 1:
+      if (populacao1 > populacao2) {
         printf("Cidade %s tem a maior população: %d \n", nome1, populacao1);
         } else {
         printf("Cidade %s tem a maior população: %d \n", nome2, populacao2);
      }
-
-     if (area1 > area2) {
+      break;
+   case 2:
+      if (area1 > area2) {
         printf("Cidade %s tem a maior area: %.2f \n", nome1, area1);
         } else {
         printf("Cidade %s tem a maior area: %.2f \n", nome2, area2);
      }
-
-     if (pib1 > pib2) {
+      break;
+   case 3:
+      if (pib1 > pib2) {
         printf("Cidade %s tem o maior pib: %.2f \n", nome1, pib1);
         } else {
         printf("Cidade %s tem o maior pib: %.2f \n", nome2, pib2);
      }
-
-     if (pontosturisticos1 > pontosturisticos2) {
+      break;
+   case 4:
+      if (pontosturisticos1 > pontosturisticos2) {
         printf("Cidade %s tem mais pontos turisticos: %d \n", nome1, pontosturisticos1);
         } else {
         printf("Cidade %s tem mais pontos turisticos: %d \n", nome2, pontosturisticos2);
      }
-
-     if (DensidadePopulacional1 < DensidadePopulacional2) {
+      break;
+   case 5:
+      if (DensidadePopulacional1 < DensidadePopulacional2) {
         printf("Cidade %s tem a melhor Densidade Populacional: %.2f \n", nome1, DensidadePopulacional1);
         } else {
         printf("Cidade %s tem a melhor Densidade Populacional: %.2f \n", nome2, DensidadePopulacional2);
      }
-
-     if (PibPerCapita1 > PibPerCapita2) {
+      break;
+   case 6:
+      if (PibPerCapita1 > PibPerCapita2) {
         printf("Cidade %s tem o maior pib per capita: %.2f \n", nome1, PibPerCapita1);
         } else {
         printf("Cidade %s tem o maior pib per capita: %.2f \n", nome2, PibPerCapita2);
      }
-
-     if (SuperPoder1 > SuperPoder2) {
+      break;   
+   case 7:
+      if (SuperPoder1 > SuperPoder2) {
         printf("O Super Poder da Cidade %s é o maior: %.2f \n", nome1, SuperPoder1);
         } else {
         printf("O Super Poder da Cidade %s é o maior: %.2f \n", nome2, SuperPoder2);
      }
+      break;
 
+
+   default:
+      printf("Opção inválida!\n");
+      break;
+   }
     return 0;
+
 }
